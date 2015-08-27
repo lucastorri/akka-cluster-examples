318b2691693c77f045d8f23ff94e2030e1cf63c0
Lucas Torri <lucastorri@gmail.com>
2015-08-27 13:36:00 +0200

    crdt example

--- src/main/scala/com/github/lucastorri/akka/demo/cluster/patterns/CRDT.scala
+++ src/main/scala/com/github/lucastorri/akka/demo/cluster/patterns/CRDT.scala
@@ -1,8 +1,80 @@
 package com.github.lucastorri.akka.demo.cluster.patterns
 
-//http://www.gearpump.io/site/0.6/internal/#master-high-availability
-//https://github.com/patriknw/akka-data-replication
-//https://www.typesafe.com/activator/template/akka-sample-distributed-data-scala
+import akka.actor.{Props, ActorSystem, ActorRef, Actor}
+import akka.actor.Actor.Receive
+import akka.cluster.Cluster
+import akka.cluster.ddata.Replicator._
+import akka.cluster.ddata.{ORSet, ORSetKey, DistributedData}
+import com.github.lucastorri.akka.demo.cluster.ClusterSeed
+import com.github.lucastorri.akka.demo.cluster.patterns.CRDT.{Remove, Add}
+import com.typesafe.config.ConfigFactory
+
+import scala.util.Random
+
+/** Notes:
+  *
+  * http://doc.akka.io/docs/akka/snapshot/scala/distributed-data.html
+  */
 object CRDT {
-  //TODO
+
+  def start(port: Int): (ActorRef, ActorSystem) = {
+    val config = ConfigFactory.parseString(
+      s"""
+         |akka.actor.provider = "akka.cluster.ClusterActorRefProvider"
+         |
+         |akka.remote.netty.tcp.port = $port
+         |akka.remote.netty.tcp.hostname = 127.0.0.1
+         |
+         |akka.cluster.seed-nodes = ["akka.tcp://${ClusterSeed.name}@127.0.0.1:${ClusterSeed.port}"]
+         |akka.cluster.auto-down-unreachable-after = 10s
+       """.stripMargin)
+    val system = ActorSystem(ClusterSeed.name, config)
+
+    val actor = system.actorOf(Props[Data])
+
+    actor -> system
+  }
+
+  class Data extends Actor {
+
+    implicit val node = Cluster(context.system)
+    val replicator = DistributedData(context.system).replicator
+    val Key = ORSetKey[Int]("key-name")
+
+    override def preStart(): Unit = {
+      replicator ! Subscribe(Key, self)
+    }
+
+    override def receive: Receive = {
+      case c @ Changed(Key) =>
+        println(s"changes ${c.get(Key).elements}")
+      case Add(n) =>
+        replicator ! Update(Key, ORSet.empty[Int], WriteLocal)(_ + n)
+      case Remove(n) =>
+        replicator ! Update(Key, ORSet.empty[Int], WriteLocal)(_ - n)
+      case msg =>
+        println(s"other $msg")
+    }
+
+  }
+
+  case class Add(n: Int)
+  case class Remove(n: Int)
+}
+
+object CRDTMain extends App {
+
+  ClusterSeed.start
+
+  val actors = (1 to 5).map(n => CRDT.start(6660 + n)._1)
+
+  def random: ActorRef =
+    actors(Random.nextInt(actors.size))
+
+  random ! Add(3)
+  random ! Add(2)
+  Thread.sleep(5000)
+  random ! Remove(3)
+  random ! Add(5)
+
 }
